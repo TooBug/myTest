e23b0f7044482cc5bdca167494addb0206fbfe5c
TooBug <i@toobug.net>
2013-01-15 13:30:49 +0800

    让测试全部通过

--- unit-test/1.spec.js
+++ unit-test/1.spec.js
@@ -2,7 +2,10 @@ describe('第一次单元测试',function(){
 	it('算数的结果总是比较容易预测',function(){
 		expect(1+2).toEqual(3);
 	});
-	it('再测一下不通过的',function(){
+	/*it('再测一下不通过的',function(){
 		expect(1+2).toEqual(2);
+	});*/
+	it('全部通过了算了吧……',function(){
+		expect(1+1).toEqual(2);
 	});
 });
